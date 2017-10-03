769f63e2f80acf5ab0390a9a9e6b9ea0a35e65ca
Satoshi Kubo <skubo@bbs.co.jp>
2017-10-03 14:56:13 +0900

    translate.

--- src/main/java/javax/servlet/ServletResponse.java
+++ src/main/java/javax/servlet/ServletResponse.java
@@ -185,7 +185,8 @@ public interface ServletResponse {
      * {@link #setContentType}を<code>text/html; charset=UTF-8</code>という<code>String</code>で呼び出すことと同等です。
      * <p>このメソッドは文字エンコーディングを変更するために繰り返し呼び出すことが出来ます。
      * このメソッドは<code>getWriter</code>が呼び出された後もしくはレスポンスがコミットされた後に呼び出しても何も行いません。
-     * <p>コンテナはプロトコルが提供している方法でサーブレットレスポンスのwriterに使用されている文字エンコーディングをクライアントに送信する必要があります。
+     * <p>プロトコルが文字エンコーディングをクライアントに送信する方法を提供している場合、
+     * コンテナはサーブレットレスポンスのwriterに使用されている文字エンコーディングをクライアントに送信する必要があります。
      * HTTPの場合は文字エンコーディングはテキストのメディアタイプにおいて<code>Content-Type</code>ヘッダーの一部分として伝えられます。
      * サーブレットでコンテンツタイプが指定されていない場合、文字エンコーディングはHTTPヘッダーを介して伝えられないことに注意してください。 
      * しかしながらサーブレットレスポンスのwriterを使用して書き込まれたテキストをエンコードするために使用されています。
@@ -223,7 +224,8 @@ public interface ServletResponse {
      * <p>このメソッドはコンテンツタイプと文字エンコーディングを変更するために繰り返し呼び出されることがあります。
      * このメソッドはレスポンスがコミットされた後に呼び出しても何も行いません。
      * このメソッドは<code>getWriter</code>が呼び出された後もしくはレスポンスがコミットされた後に呼び出しても文字エンコーディングの設定を行いません。
-     * <p>コンテナはプロトコルが提供している方法でサーブレットレスポンスのwriterに使用されている文字エンコーディングをクライアントに送信する必要があります。
+     * <p>プロトコルが文字エンコーディングをクライアントに送信する方法を提供している場合、
+     * コンテナはサーブレットレスポンスのwriterに使用されている文字エンコーディングをクライアントに送信する必要があります。
      * HTTPの場合は文字エンコーディングはテキストのメディアタイプにおいて<code>Content-Type</code>ヘッダーの一部分として伝えられます。
      *
      * @param type コンテンツのMIMEタイプを示す<code>String</code>
@@ -343,21 +345,13 @@ public interface ServletResponse {
      * そうでない場合はロケールから文字文字エンコーディングへのマッピングはコンテナに依存します。
      * <p>このメソッドは文字エンコーディングを変更するために繰り返し呼び出されることがあります。
      * このメソッドはレスポンスがコミットされた後に呼び出しても何も行いません。
-     * It does not set the response's
-     * character encoding if it is called after {@link #setContentType}
-     * has been called with a charset specification, after
-     * {@link #setCharacterEncoding} has been called, after
-     * <code>getWriter</code> has been called, or after the response
-     * has been committed.
-     * <p>Containers must communicate the locale and the character encoding
-     * used for the servlet response's writer to the client if the protocol
-     * provides a way for doing so. In the case of HTTP, the locale is
-     * communicated via the <code>Content-Language</code> header,
-     * the character encoding as part of the <code>Content-Type</code>
-     * header for text media types. Note that the character encoding
-     * cannot be communicated via HTTP headers if the servlet does not
-     * specify a content type; however, it is still used to encode text
-     * written via the servlet response's writer.
+     * このメソッドはすでに{@link #setContentType}がcharsetの指定を伴う呼び出しをされている場合、{@link #setCharacterEncoding}が呼び出されている場合、
+     * <code>getWriter</code>が呼び出されている場合、レスポンスがコミットされている場合は、レスポンスに文字エンコーディングの設定を行いません。
+     * <p>プロトコルが文字エンコーディングをクライアントに送信する方法を提供している場合、
+     * コンテナはサーブレットレスポンスのwriterに使用されている文字エンコーディングをクライアントに送信する必要があります。
+     * HTTPの場合は文字エンコーディングはテキストのメディアタイプにおいて<code>Content-Type</code>ヘッダーの一部分として伝えられます。
+     * サーブレットでコンテンツタイプが指定されていない場合、文字エンコーディングはHTTPヘッダーを介して伝えられないことに注意してください。 
+     * しかしながらサーブレットレスポンスのwriterを使用して書き込まれたテキストをエンコードするために使用されています。
      * 
      * @param loc レスポンスのロケール
      *
